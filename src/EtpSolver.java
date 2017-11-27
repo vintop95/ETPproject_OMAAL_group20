d8474798c50bef9d21e542b7197b7857b0514b8c
vintop95 <vincenzotop95@gmail.com>
2017-11-27 02:47:42 +0100

    First steps

---
+++ src/EtpSolver.java
@@ -0,0 +1,34 @@
+import java.util.Vector;
+
+class Student{
+	int studentId;
+	Vector<int> examsEnrolled;
+}
+
+public class EtpSolver {
+
+	static void generateConflicts(int[][] n){
+		
+		Vector<Student> student = new Vector<Student>();
+	}
+	
+	public static void main(String[] args) {
+		
+		//initialize counting the rows of instanceXX.exm
+		final int N_EXAMS;
+		
+		//load from instanceXX.slo
+		final int N_TIMESL;
+		
+		//Conflict matrix
+		//may be too large
+		int[][] n = new int[N_EXAMS][N_EXAMS];
+				
+		//load from instanceXX.stu 
+		generateConflicts(n);
+		
+		
+
+	}
+
+}
